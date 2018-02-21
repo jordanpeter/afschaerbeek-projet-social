fc06820401d68faa89def05a6fed655d77b6d529
jordanpeter <jordanpeter26@gmail.com>
2018-02-21 16:16:34 +0100

    v1 k

---
+++ assets/js/buttontothetop.js
@@ -0,0 +1,21 @@
+var myicon = document.createElement("button");
+myicon.style.display = "none";
+
+document.addEventListener("scroll",function(event){
+
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+
+  myicon.classList.add("fa","fa-3x","fa-arrow-circle-o-up"); //pour l'icone fontawesome
+  myicon.id = 'buttonScroll'; //pour le style et le placement
+  document.body.appendChild(myicon); // l'ajouter au <body> du fichier HTML
+  myicon.style.display = "block"; //si scroll de 20px affiche la div
+} else {
+
+  myicon.style.display = "none"; // cache la div
+}
+myicon.addEventListener("click", function(event){ //oui un eventlistener dans un eventlistener
+document.body.scrollTop = 0; // event-ception par code-caprio xD
+document.documentElement.scrollTop=0;
+myicon.parentNode.removeChild(myicon);
+}); // click reviens au top
+});
--- assets/js/icones.js
+++ assets/js/icones.js
@@ -3,13 +3,18 @@
       $('.services').hide();
       $('.tarifs').hide();
       $('.petitmot').hide();
+      $('.acces').hide();
       // mission
       $('#iconclick1').on('click', function(){
            $('.asbl').hide('fast');
            $('.services').hide('fast');
            $('.tarifs').hide('fast');
            $('.petitmot').hide('fast');
+           $('.acces').hide('fast');
            $('.mission').show('slow');
+           $('.iconclick').removeClass("black");
+           $('.style1').addClass("black");
+});
       })
       // asbl
       $('#iconclick2').on('click', function(){
@@ -17,7 +22,10 @@
            $('.tarifs').hide('fast');
            $('.mission').hide('fast');
            $('.petitmot').hide('fast');
+           $('.acces').hide('fast');
            $('.asbl').show('slow');
+           $('.iconclick').removeClass("black");
+           $('.style22').addClass("black"); // Style 22 because style2 make the background white when you click on the (this) icon.
       })
       //services
       $('#iconclick3').on('click', function(){
@@ -25,20 +33,43 @@
            $('.mission').hide('fast');
            $('.asbl').hide('fast');
            $('.petitmot').hide('fast');
+           $('.acces').hide('fast');
            $('.services').show('slow');
+           $('.iconclick').removeClass("black");
+           $('.style3').addClass("black");
       })
       $('#iconclick4').on('click', function(){
           $('.mission').hide('fast');
           $('.asbl').hide('fast');
           $('.services').hide('fast');
           $('.petitmot').hide('fast');
+          $('.acces').hide('fast');
           $('.tarifs').show('slow');
+          $('.iconclick').removeClass("black");
+         $('.style4').addClass("black");
      })
      $('#iconclick5').on('click', function(){
          $('.mission').hide('fast');
          $('.asbl').hide('fast');
          $('.services').hide('fast');
          $('.tarifs').hide('fast');
+         $('.acces').hide('fast');
          $('.petitmot').show('slow');
+         $('.iconclick').removeClass("black");
+        $('.style5').addClass("black");
     })
-})
+    $('#iconclick6').on('click', function(){
+        $('.mission').hide('fast');
+        $('.asbl').hide('fast');
+        $('.services').hide('fast');
+        $('.tarifs').hide('fast');
+        $('.petitmot').hide('fast');
+        $('.acces').show('slow');
+        $('.iconclick').removeClass("black");
+        $('.style6').addClass("black");
+   })
+   $( ".iconclick" ).click(function() {
+ $( this ).addClass( "black" );
+
+ 
+ })
